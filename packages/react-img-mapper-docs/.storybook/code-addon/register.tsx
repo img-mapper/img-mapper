cbc0ea362a4314dc1e6886da96b9ed5bb2f495b2
Nisharg Shah <nishargshah3101@gmail.com>
2025-10-08 03:45:21 +0530

    fix: issues

--- packages/react-img-mapper-docs/.storybook/code-addon/register.tsx
+++ packages/react-img-mapper-docs/.storybook/code-addon/register.tsx
@@ -1,12 +1,13 @@
 import { AddonPanel } from 'storybook/internal/components';
 import { addons, types } from 'storybook/manager-api';
 
-import React, { FC } from 'react';
+// DON'T REMOVE REACT FROM HERE
+import React from 'react';
 import { useParameter } from 'storybook/manager-api';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-const Content: FC = () => {
+const Content = () => {
   const code = useParameter('code', 'No Code Available');
 
   return (
