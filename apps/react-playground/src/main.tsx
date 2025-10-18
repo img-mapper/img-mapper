fb62f0918f763a7cb04d7dd1b7f1d674c31dae1d
Nisharg Shah <nishargshah3101@gmail.com>
2025-10-18 18:16:24 +0530

    fix: play ground removed

--- apps/react-playground/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import { createRoot } from 'react-dom/client';
-
-import App from '@playground/App';
-
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-createRoot(document.querySelector('#root')!).render(<App />);
