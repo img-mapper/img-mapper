5acfe6f93ddbf4214cb821bfa02f480afec1df70
Nisharg Shah <nishargshah3101@gmail.com>
2025-10-18 14:04:56 +0530

    fix: linting issue resolved

--- apps/react-img-mapper-docs/src/stories/Area.stories.tsx
+++ apps/react-img-mapper-docs/src/stories/Area.stories.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 import {
   clearSelectedHighlightedAreaCode,
   disabledAreaCode,
@@ -13,6 +11,7 @@ import {
 } from '@/code/areas';
 import Mapper from '@/components/Mapper';
 import TopComponent from '@/components/TopComponent';
+import ZoomInZoomOutAreaComp from '@/components/ZoomInZoomOutAreaComp';
 
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
@@ -265,44 +264,7 @@ export const ToggleStayHighlightedArea: Story = {
 };
 
 export const ZoomInZoomOutArea: Story = {
-  render: (args) => {
-    const minWidth = 400;
-    const { parentWidth = 100 } = args;
-
-    const [zoom, setZoom] = useState(640);
-
-    const handleZoom = (type: 'in' | 'out') => {
-      setZoom((prev) => {
-        if (prev <= minWidth && type === 'out') return prev;
-        return type === 'in' ? prev + parentWidth : prev - parentWidth;
-      });
-    };
-
-    return (
-      <Mapper
-        responsive
-        parentWidth={zoom}
-        TopComponent={() =>
-          TopComponent(
-            'Zoom In & Zoom Out Area',
-            <p>
-              In this example, Zoom is based on the <span className="tag">parentWidth</span> that
-              you can change by the <span className="tag">control tab</span>, you can press the
-              below buttons to see the <span className="tag">live</span> results in image mapper
-              <br />
-              <br />
-              <button onClick={() => handleZoom('in')} style={{ marginRight: 8 }} type="button">
-                Zoom In
-              </button>
-              <button onClick={() => handleZoom('out')} type="button">
-                Zoom Out
-              </button>
-            </p>,
-          )
-        }
-      />
-    );
-  },
+  render: ({ parentWidth }) => <ZoomInZoomOutAreaComp parentWidth={parentWidth} />,
   parameters: {
     code: zoomInZoomOutAreaCode,
   },
